d6c09129a8440d13feaba92c600691e636aa1233
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2026-09-01 19:07:38 -0300

    Added more logic to Player

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,10 +1,19 @@
 #include "Player.hpp"
+#include <raylib.h>
 
 Player::Player(Vector2 startPosition, float size, float speed, Color color)
     : position(startPosition), size(size), speed(speed), color(color) {}
 
 void Player::Update(float deltaTime) {
   float distance = speed * deltaTime;
+
+  if (IsKeyPressed(KEY_SPACE))
+    color = ColorIsEqual(color, DARKGREEN) ? MAROON : DARKGREEN;
+
+  if (IsKeyPressed(KEY_LEFT_SHIFT))
+    speed += 300;
+  if (IsKeyReleased(KEY_LEFT_SHIFT))
+    speed -= 300;
 
   if (IsKeyDown(KEY_D))
     position.x += distance;
